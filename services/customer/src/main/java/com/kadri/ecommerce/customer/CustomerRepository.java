af831565c1a8f3f3639380faa72a94892926b4fb
kadri <kadriyaakoub@gmail.com>
2025-10-01 23:35:34 -0500

    First setup, Discovery and Customer services

---
+++ services/customer/src/main/java/com/kadri/ecommerce/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.kadri.ecommerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+}
